730c2d8ea6b27eb25c2a9b217a7157c4bf7ca394
Sardut13 <epsilon7611@gmail.com>
2026-03-27 19:54:45 +0100

    Create rickroll.js

---
+++ rickroll.js
@@ -0,0 +1,8 @@
+let num = Math.round((Math.random() * 1))
+
+if (num == 0) {
+    window.location = "https://youtu.be/dQw4w9WgXcQ"
+}
+else if (num == 1) {
+    window.location = "https://www.masecurite.interieur.gouv.fr/fr/fiches-pratiques/numerique/cyberattaques-comment-dejouer-phishing-qr-codes-sms-frauduleux-spoofing"
+}
